95a7cabe9db99678813c6d9632a28c3788c1c2bc
yuyang <18210033490@163.com>
2019-07-19 15:25:39 +0800

    【修改】修改请求路径

--- static/js/util.js
+++ static/js/util.js
@@ -1,4 +1,4 @@
-function getCMDB(url='/api/c/self-service-api/snc/cmdb/getcmdbinstancelist/') {
+function getCMDB(url='/api/c/self-service-api/snc/cmdb/getcmdbinstancelist') {
     let params = {"params":{"condition":{"ciId":77,"condition":""},"pagination":{"currentPage":1,"pageSize":50}}}
     $.post(url, params,function(data){
         if (data.recordList) {

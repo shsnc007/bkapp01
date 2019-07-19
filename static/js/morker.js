8f1d1d6113ab5eb1ec5eb02dc39c39870b9ad428
yuyang <18210033490@163.com>
2019-07-19 18:23:35 +0800

    【新增】 新增接口

---
+++ static/js/morker.js
@@ -0,0 +1,572 @@
+const data = [
+    {
+        "instanceId": 89,
+        "soluteStatus": null,
+        "instanceName": "50167-test#192.168.50.167",
+        "columns": [
+            {
+                "alias": null,
+                "code": "id",
+                "name": "ID",
+                "value": 89
+            },
+            {
+                "alias": null,
+                "code": "instance_name",
+                "name": "实例名称",
+                "value": "50167-test#192.168.50.167"
+            },
+            {
+                "alias": null,
+                "code": "solute_status",
+                "name": "变更状态",
+                "value": "审核通过"
+            },
+            {
+                "alias": null,
+                "code": "host_name",
+                "name": "主机名",
+                "value": "50167-test"
+            },
+            {
+                "alias": null,
+                "code": "ip",
+                "name": "IP",
+                "value": "192.168.50.167"
+            },
+            {
+                "alias": null,
+                "code": "vip",
+                "name": "VIP",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "manage_ip",
+                "name": "管理IP",
+                "value": "192.168.50.167"
+            },
+            {
+                "alias": null,
+                "code": "server_type",
+                "name": "服务器类型",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "os_version",
+                "name": "操作系统版本",
+                "value": "CentOS Linux release 7.6.1810 (Core)"
+            },
+            {
+                "alias": null,
+                "code": "os_type",
+                "name": "操作系统类型",
+                "value": "Linux"
+            },
+            {
+                "alias": null,
+                "code": "use_state",
+                "name": "使用状态",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "82_14_serial_number",
+                "name": "安装于x86服务器的序列号",
+                "value": null
+            }
+        ]
+    },
+    {
+        "instanceId": 88,
+        "soluteStatus": null,
+        "instanceName": "50166-test#192.168.50.166",
+        "columns": [
+            {
+                "alias": null,
+                "code": "id",
+                "name": "ID",
+                "value": 88
+            },
+            {
+                "alias": null,
+                "code": "instance_name",
+                "name": "实例名称",
+                "value": "50166-test#192.168.50.166"
+            },
+            {
+                "alias": null,
+                "code": "solute_status",
+                "name": "变更状态",
+                "value": "审核通过"
+            },
+            {
+                "alias": null,
+                "code": "host_name",
+                "name": "主机名",
+                "value": "50166-test"
+            },
+            {
+                "alias": null,
+                "code": "ip",
+                "name": "IP",
+                "value": "192.168.50.166"
+            },
+            {
+                "alias": null,
+                "code": "vip",
+                "name": "VIP",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "manage_ip",
+                "name": "管理IP",
+                "value": "192.168.50.166"
+            },
+            {
+                "alias": null,
+                "code": "server_type",
+                "name": "服务器类型",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "os_version",
+                "name": "操作系统版本",
+                "value": "CentOS Linux release 7.4.1708 (Core)"
+            },
+            {
+                "alias": null,
+                "code": "os_type",
+                "name": "操作系统类型",
+                "value": "Linux"
+            },
+            {
+                "alias": null,
+                "code": "use_state",
+                "name": "使用状态",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "82_14_serial_number",
+                "name": "安装于x86服务器的序列号",
+                "value": null
+            }
+        ]
+    },
+    {
+        "instanceId": 86,
+        "soluteStatus": null,
+        "instanceName": "50164-yanshi-nginx-bieguan#192.168.50.164#192.168.50.164",
+        "columns": [
+            {
+                "alias": null,
+                "code": "id",
+                "name": "ID",
+                "value": 86
+            },
+            {
+                "alias": null,
+                "code": "instance_name",
+                "name": "实例名称",
+                "value": "50164-yanshi-nginx-bieguan#192.168.50.164#192.168.50.164"
+            },
+            {
+                "alias": null,
+                "code": "solute_status",
+                "name": "变更状态",
+                "value": "审核通过"
+            },
+            {
+                "alias": null,
+                "code": "host_name",
+                "name": "主机名",
+                "value": "50164-yanshi-nginx-bieguan"
+            },
+            {
+                "alias": null,
+                "code": "ip",
+                "name": "IP",
+                "value": "192.168.50.164"
+            },
+            {
+                "alias": null,
+                "code": "vip",
+                "name": "VIP",
+                "value": "192.168.50.164"
+            },
+            {
+                "alias": null,
+                "code": "manage_ip",
+                "name": "管理IP",
+                "value": "192.168.50.164"
+            },
+            {
+                "alias": null,
+                "code": "server_type",
+                "name": "服务器类型",
+                "value": "PC服务器"
+            },
+            {
+                "alias": null,
+                "code": "os_version",
+                "name": "操作系统版本",
+                "value": "CentOS Linux release 7.4.1708 (Core)"
+            },
+            {
+                "alias": null,
+                "code": "os_type",
+                "name": "操作系统类型",
+                "value": "Linux"
+            },
+            {
+                "alias": null,
+                "code": "use_state",
+                "name": "使用状态",
+                "value": "上线"
+            },
+            {
+                "alias": null,
+                "code": "82_14_serial_number",
+                "name": "安装于x86服务器的序列号",
+                "value": null
+            }
+        ]
+    },
+    {
+        "instanceId": 90,
+        "soluteStatus": null,
+        "instanceName": "50163-yanshi-proxy-bieguan#192.168.50.163",
+        "columns": [
+            {
+                "alias": null,
+                "code": "id",
+                "name": "ID",
+                "value": 90
+            },
+            {
+                "alias": null,
+                "code": "instance_name",
+                "name": "实例名称",
+                "value": "50163-yanshi-proxy-bieguan#192.168.50.163"
+            },
+            {
+                "alias": null,
+                "code": "solute_status",
+                "name": "变更状态",
+                "value": "审核通过"
+            },
+            {
+                "alias": null,
+                "code": "host_name",
+                "name": "主机名",
+                "value": "50163-yanshi-proxy-bieguan"
+            },
+            {
+                "alias": null,
+                "code": "ip",
+                "name": "IP",
+                "value": "192.168.50.163"
+            },
+            {
+                "alias": null,
+                "code": "vip",
+                "name": "VIP",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "manage_ip",
+                "name": "管理IP",
+                "value": "192.168.50.163"
+            },
+            {
+                "alias": null,
+                "code": "server_type",
+                "name": "服务器类型",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "os_version",
+                "name": "操作系统版本",
+                "value": "CentOS Linux release 7.4.1708 (Core)"
+            },
+            {
+                "alias": null,
+                "code": "os_type",
+                "name": "操作系统类型",
+                "value": "Linux"
+            },
+            {
+                "alias": null,
+                "code": "use_state",
+                "name": "使用状态",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "82_14_serial_number",
+                "name": "安装于x86服务器的序列号",
+                "value": null
+            }
+        ]
+    },
+    {
+        "instanceId": 85,
+        "soluteStatus": null,
+        "instanceName": "test#192.168.50.171",
+        "columns": [
+            {
+                "alias": null,
+                "code": "id",
+                "name": "ID",
+                "value": 85
+            },
+            {
+                "alias": null,
+                "code": "instance_name",
+                "name": "实例名称",
+                "value": "test#192.168.50.171"
+            },
+            {
+                "alias": null,
+                "code": "solute_status",
+                "name": "变更状态",
+                "value": "审核通过"
+            },
+            {
+                "alias": null,
+                "code": "host_name",
+                "name": "主机名",
+                "value": "test"
+            },
+            {
+                "alias": null,
+                "code": "ip",
+                "name": "IP",
+                "value": "192.168.50.171"
+            },
+            {
+                "alias": null,
+                "code": "vip",
+                "name": "VIP",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "manage_ip",
+                "name": "管理IP",
+                "value": "192.168.50.171"
+            },
+            {
+                "alias": null,
+                "code": "server_type",
+                "name": "服务器类型",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "os_version",
+                "name": "操作系统版本",
+                "value": "CentOS release 6.7 (Final)"
+            },
+            {
+                "alias": null,
+                "code": "os_type",
+                "name": "操作系统类型",
+                "value": "Linux"
+            },
+            {
+                "alias": null,
+                "code": "use_state",
+                "name": "使用状态",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "82_14_serial_number",
+                "name": "安装于x86服务器的序列号",
+                "value": null
+            }
+        ]
+    },
+    {
+        "instanceId": 79,
+        "soluteStatus": null,
+        "instanceName": "192.168.50.42#192.168.50.42#192.168.50.42",
+        "columns": [
+            {
+                "alias": null,
+                "code": "id",
+                "name": "ID",
+                "value": 79
+            },
+            {
+                "alias": null,
+                "code": "instance_name",
+                "name": "实例名称",
+                "value": "192.168.50.42#192.168.50.42#192.168.50.42"
+            },
+            {
+                "alias": null,
+                "code": "solute_status",
+                "name": "变更状态",
+                "value": "审核通过"
+            },
+            {
+                "alias": null,
+                "code": "host_name",
+                "name": "主机名",
+                "value": "192.168.50.42"
+            },
+            {
+                "alias": null,
+                "code": "ip",
+                "name": "IP",
+                "value": "192.168.50.42"
+            },
+            {
+                "alias": null,
+                "code": "vip",
+                "name": "VIP",
+                "value": "192.168.50.42"
+            },
+            {
+                "alias": null,
+                "code": "manage_ip",
+                "name": "管理IP",
+                "value": "192.168.50.42"
+            },
+            {
+                "alias": null,
+                "code": "server_type",
+                "name": "服务器类型",
+                "value": "PC服务器"
+            },
+            {
+                "alias": null,
+                "code": "os_version",
+                "name": "操作系统版本",
+                "value": ""
+            },
+            {
+                "alias": null,
+                "code": "os_type",
+                "name": "操作系统类型",
+                "value": "AIX"
+            },
+            {
+                "alias": null,
+                "code": "use_state",
+                "name": "使用状态",
+                "value": "上线"
+            },
+            {
+                "alias": null,
+                "code": "82_14_serial_number",
+                "name": "安装于x86服务器的序列号",
+                "value": null
+            }
+        ]
+    },
+    {
+        "instanceId": 72,
+        "soluteStatus": null,
+        "instanceName": "50221-yanshi#192.168.50.221#192.168.50.221",
+        "columns": [
+            {
+                "alias": null,
+                "code": "id",
+                "name": "ID",
+                "value": 72
+            },
+            {
+                "alias": null,
+                "code": "instance_name",
+                "name": "实例名称",
+                "value": "50221-yanshi#192.168.50.221#192.168.50.221"
+            },
+            {
+                "alias": null,
+                "code": "solute_status",
+                "name": "变更状态",
+                "value": "审核通过"
+            },
+            {
+                "alias": null,
+                "code": "host_name",
+                "name": "主机名",
+                "value": "50221-yanshi"
+            },
+            {
+                "alias": null,
+                "code": "ip",
+                "name": "IP",
+                "value": "192.168.50.221"
+            },
+            {
+                "alias": null,
+                "code": "vip",
+                "name": "VIP",
+                "value": "192.168.50.221"
+            },
+            {
+                "alias": null,
+                "code": "manage_ip",
+                "name": "管理IP",
+                "value": "192.168.50.221"
+            },
+            {
+                "alias": null,
+                "code": "server_type",
+                "name": "服务器类型",
+                "value": "PC服务器"
+            },
+            {
+                "alias": null,
+                "code": "os_version",
+                "name": "操作系统版本",
+                "value": "CentOS Linux release 7.4.1708 (Core)"
+            },
+            {
+                "alias": null,
+                "code": "os_type",
+                "name": "操作系统类型",
+                "value": "Linux"
+            },
+            {
+                "alias": null,
+                "code": "use_state",
+                "name": "使用状态",
+                "value": "上线"
+            },
+            {
+                "alias": null,
+                "code": "82_14_serial_number",
+                "name": "安装于x86服务器的序列号",
+                "value": null
+            }
+        ]
+    }
+];
+let row = [];
+data.map((item)=>{
+    let columns = {};
+    item.columns.map((items,index) => {
+        columns[items.code] = items.value;
+    })
+    row.push(columns);
+})
+console.log(row);
+
+function formatData(row) {
+    
+    row.map((item,index) => {
+        
+    })
+}
+formatData();

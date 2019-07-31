01ba44c195d9437bcd6ade92294c63a265e9a17b
jmsyy <jmsyuyang777@gmail.com>
2019-07-31 22:20:48 +0800

    【修改】修改告警与性能列表参数

--- static/js/util.js
+++ static/js/util.js
@@ -24,9 +24,11 @@ const paramsUnit = [
      // 编辑CMDB主机
     {params:{"ciId":77,"instanceRecord":{"attributes":[{"attributeId":1301,"attributeValue":"50167-test"},{"attributeId":1302,"attributeValue":"192.168.50.167"},{"attributeId":1303,"attributeValue":null},{"attributeId":1304,"attributeValue":"192.168.50.167"},{"attributeId":1306,"attributeValue":null},{"attributeId":1309,"attributeValue":"Linux"},{"attributeId":1307,"attributeValue":"192.168.50.1"},{"attributeId":1308,"attributeValue":"CentOS Linux release 7.6.1810 (Core)"},{"attributeId":1310,"attributeValue":"3.10.0-957.21.3.el7.x86_64"},{"attributeId":1311,"attributeValue":3790},{"attributeId":1312,"attributeValue":200},{"attributeId":1313,"attributeValue":0},{"attributeId":1305,"attributeValue":"192.168.50.167"},{"attributeId":1314,"attributeValue":null},{"attributeId":1300,"attributeValue":"host_10000059"},{"attributeId":309,"attributeValue":"yanshi1"},{"attributeId":310,"attributeValue":1562322493634},{"attributeId":311,"attributeValue":"yanshi1"},{"attributeId":312,"attributeValue":1563418366189}],"relations":[{"quoteRelation":{"quoteCiId":84,"relationId":8},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":82,"relationId":14},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":87,"relationId":8},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":86,"relationId":8},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":98,"relationId":27},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":90,"relationId":8},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":26,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":27,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":18,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":24,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":29,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":10,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":20,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":19,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":37,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":40,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":38,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":21,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":35,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":43,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":49,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":42,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":51,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":46,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":60,"relationId":20},"quoteInstances":[]},{"quoteRelation":{"quoteCiId":98,"relationId":25},"quoteInstances":[]}],"instanceId":89}}},
      //获取监控告警列表数据
-    {params:{"pagination":{"pagenum":1,"pagesize":40,"sort":{}},"condition":{"alarmId":"","businessIds":[],"deviceTypeIds":[],"hostIp":"","hostName":"","alarmInfo":"","resourceGroupIds":[],"itemName":"","value":"","priorityList":[],"precise":false,"hostIds":[],"handleStatus":1,"lastClockStartTime":"","lastClockEndTime":"","maintenancesFlag":""}}},
+    // {params:{"pagination":{"pagenum":1,"pagesize":40,"sort":{}},"condition":{"alarmId":"","businessIds":[],"deviceTypeIds":[],"hostIp":"","hostName":"","alarmInfo":"","resourceGroupIds":[],"itemName":"","value":"","priorityList":[],"precise":false,"hostIds":[],"handleStatus":1,"lastClockStartTime":"","lastClockEndTime":"","maintenancesFlag":""}}},
+    {"params":{"pagination":{"pagenum":1,"pagesize":40,"sort":{}},"condition":{"alarmId":"","businessIds":[],"deviceTypeIds":[],"hostIp":"","hostName":"","alarmInfo":"","resourceGroupIds":[],"itemName":"","value":"","priorityList":[],"precise":false,"hostIds":[],"handleStatus":1,"lastClockStartTime":"","lastClockEndTime":"","maintenancesFlag":"","searchHistoryType":[],"remindType":false}}},
     //获取监控性能最新数据
-    {params:{"pagination":{"pagenum":1,"pagesize":"10","sort":{}},"condition":{"hostid":"717"}}},
+    
+     {params:{"pagination":{"pagenum":1,"pagesize":"10","sort":{}},"condition":{"hostid":"127"}}},
     //执行操作的基本信息
     {params:{"id":2896}},
     //执行操作日志信息
@@ -40,9 +42,16 @@ $.postJSON = function( data, callback) {
     data.bk_app_secret = bk_app_secret;
     data.bk_app_code = bk_app_code;
     data.bk_username = bk_username;
-    data.amp_url = amp_url;
-    data.amp_user = amp_user;
-    data.amp_passwd = amp_passwd;
+    if (data.index) {
+        ata.amp_url = 'http://52.81.101.159.221:8083';
+        data.amp_user = 'admin';
+        data.amp_passwd = 'shsnc!@#$%^';
+    }else {
+        data.amp_url = amp_url;
+        data.amp_user = amp_user;
+        data.amp_passwd = amp_passwd;
+    }
+   
     return jQuery.ajax({
         'type': 'POST',
         'url': snc_url,
@@ -69,6 +78,9 @@ function getData(index,dataKey) {
     let params={},amp_api = getAmpApi(index);
     params.amp_api_params =paramsUnit[index];
     params.amp_api =amp_api;
+    if (index===3||index ===4) {
+        params.index = index;
+    }
 
     $.postJSON( params,function(data){
         if (data&&data.msgCode===200) {
